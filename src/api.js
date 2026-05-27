71e37b21e5eb8bc5077828917499a1fdd32a28ff
unknown <swethaganesh502@gmail.com>
2026-05-27 16:18:09 +0530

    update to render url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://bloodconnect-backend-production.up.railway.app/api';
+const API_BASE_URL = 'https://bloodconnect-java.onrender.com/api';
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
